857815f27234c13dc631ab7b11944dde68963754
smothman <89706436+smothman@users.noreply.github.com>
2021-09-15 20:16:34 -0500

    Add files via upload

---
+++ Zylabs 5.22.py
@@ -0,0 +1,75 @@
+#Sam Othman
+#1991756
+print('''Davy's auto shop services
+Oil change -- $35
+Tire rotation -- $19
+Car wash -- $7
+Car wax -- $12''')
+
+print()
+
+# second segment for prompting user for two services from menu.
+
+print('Select first service:')
+service1 = input()
+print('Select second service:')
+service2 = input()
+
+print()
+# third segment including the fourth segment too, invoice for the  services selected and if user input a '-' the we display no service.
+# I have done a little bit modification, if some user input a wrong string then we display invalid option selected.
+
+print("Daya's auto shop invoice")
+
+print()
+
+total = 0
+
+if service1 == 'Oil change':
+    print('Service 1: Oil change, $35')
+    total = total + 35
+
+elif service1 == 'Tire rotation':
+    print('Service 1: Tire rotation, $19')
+    total = total + 19
+
+elif service1 == 'Car wash':
+    print('Service 1: Car wash, $7')
+    total = total + 7
+
+elif service1 == 'Car wax':
+    print('Service 1: Car wax, $12')
+    total = total + 12
+
+elif service1 == '-':
+    print('Service 1: No service')
+    total = total + 0
+
+else:
+    print('Service 1: Invalid option')
+
+if service2 == 'Oil change':
+    print('Service 2: Oil change, $35')
+    total = total + 35
+
+elif service2 == 'Tire rotation':
+    print('Service 2: Tire rotation, $19')
+    total = total + 19
+
+elif service2 == 'Car wash':
+    print('Service 2: Car wash, $7')
+    total = total + 7
+
+elif service2 == 'Car wax':
+    print('Service 2: Car wax, $12')
+    total = total + 12
+
+elif service2 == '-':
+    print('Service 2: No service')
+    total = total + 0
+
+else:
+    print('Service 2: Invalid option')
+
+print()
+print('Total: ${}'.format(total))
